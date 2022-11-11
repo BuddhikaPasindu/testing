509ad0d1e500b4b21f6c353d9a94b2ef69c785c4
BuddhikaPasindu <117962720+BuddhikaPasindu@users.noreply.github.com>
2022-11-11 09:59:21 +0530

    Added the new file
    A file called pilling up was uploaded from the computer

---
+++ Pilling_Up.py
@@ -0,0 +1,44 @@
+T = int(input('T: '))
+
+
+for i in range(T):
+    n = int(input('n: '))
+    block = list(map(int, input('Numbers: ').split(' ')))
+
+    pile = []
+    while len(block)>0:
+        first = block[0]
+        last = block[-1]
+
+        if len(pile)==0:    
+            if (first==last):
+                pile.append(first)
+                block.pop(0)
+            elif (first>last):
+                pile.append(first)
+                block.pop(0)
+            else:
+                pile.append(last)
+                block.pop(-1)
+        else:
+            top = pile[-1]
+            if (first==last and first<=top):
+                pile.append(first)
+                block.pop(0)
+            elif (first>last and first<=top):
+                pile.append(first)
+                block.pop(0)
+            elif (last>first and last<=top):
+                pile.append(last)
+                block.pop(-1)
+            else:
+                print('No')
+                break
+    if (len(block)==0):
+        print('Yes')
+    print(pile)
+                
+                
+        
+
+
